50de1d8b3490656bd94c214a0376cdb9cbc18129
Adam SungMin Park <parksm2@uci.edu>
2021-08-10 04:41:41 +0000

    max subset

---
+++ maxSubset/main.js
@@ -0,0 +1,15 @@
+function maxSubsetSumNoAdjacent(array) {
+  // Write your code here.
+	if(array.length === 0) {
+		return 0;
+	}
+	if(array.length ===1){
+		return array[0];
+	}
+	const maxSum = array.slice();
+	maxSum[1] = Math.max(array[0],array[1]);
+	for(let i = 2 ; i < array.length ; i++){
+		maxSum[i] = Math.max(maxSum[i-1],maxSum[i-2]+array[i]);
+	}
+	return maxSum
+}
