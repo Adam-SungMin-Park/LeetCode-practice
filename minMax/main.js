25438b577e57009079950fdf962253e806ba6ad8
Adam SungMin Park <parksm2@uci.edu>
2021-10-09 06:47:26 +0000

    min max

---
+++ minMax/main.js
@@ -0,0 +1,14 @@
+function miniMaxSum(arr) {
+    // Write your code here\
+    let min =0 ;
+    let max =0 ;
+    arr = arr.sort();
+
+    for(let i =0 ; i < arr.length-1; i++){
+        min += arr[i];
+    }
+    for (let i = 1 ; i < arr.length ; i++){
+        max += arr[i];
+    }
+    console.log(min,max)
+}
