ffe1b6bdddfce1280ef56fed4a51c576b5d17bfb
Adam SungMin Park <parksm2@uci.edu>
2021-10-15 06:53:39 +0000

    steps to zero

---
+++ stepToZero/main.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+   let counter =0;
+    while(num!==0){
+        if(num%2===0){
+            num = num/2;
+            counter++;
+        }
+        if(num%2!==0){
+            num = num -1;
+            counter++;
+        }
+    }
+    return counter;
+};
